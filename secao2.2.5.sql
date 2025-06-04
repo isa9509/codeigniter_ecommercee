51120e4d18988d19606db36a23fb87a1b858a011
isa9509 <patriciaisabella950@gmail.com>
2025-06-04 13:44:55 -0300

    Atualizar o secao sql

--- secao2.2.5.sql
+++ secao2.2.5.sql
@@ -1,8 +1,6 @@
--- MySQL Administrator dump 1.4
---
+-- Dump do administrador MySQL 1.4
 -- ------------------------------------------------------
--- Server version	5.6.16-log
-
+-- Versão do servidor 5.6.16-log
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -13,53 +11,41 @@
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 
-
---
--- Create schema loja
---
-
+-- Criar esquema loja
 CREATE DATABASE IF NOT EXISTS loja;
 USE loja;
 
---
--- Definition of table `clientes`
---
-
+-- Definição da tabela `clientes`
 DROP TABLE IF EXISTS `clientes`;
+
 CREATE TABLE `clientes` (
-  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `nome` varchar(245) NOT NULL,
-  `sobrenome` varchar(145) NOT NULL,
-  `rg` varchar(45) NOT NULL,
-  `cpf` varchar(45) NOT NULL,
-  `data_nascimento` datetime NOT NULL,
-  `sexo` char(1) NOT NULL,
-  `rua` varchar(145) NOT NULL,
-  `numero` varchar(15) NOT NULL,
-  `bairro` varchar(145) NOT NULL,
-  `cidade` varchar(145) NOT NULL,
-  `estado` varchar(45) NOT NULL,
-  `cep` varchar(15) NOT NULL,
-  `telefone` varchar(15) NOT NULL,
-  `celular` varchar(15) NOT NULL,
-  `email` varchar(145) NOT NULL,
-  `senha` varchar(45) NOT NULL,
-  `status` tinyint(1) NOT NULL DEFAULT '0',
-  `cadastrado_em` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `id` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `nome` VARCHAR(245) NOT NULL,
+  `sobrenome` VARCHAR(145) NOT NULL,
+  `rg` VARCHAR(45) NOT NULL,
+  `cpf` VARCHAR(45) NOT NULL,
+  `data_nascimento` DATETIME NOT NULL,
+  `sexo` CHAR(1) NOT NULL,
+  `rua` VARCHAR(145) NOT NULL,
+  `numero` VARCHAR(15) NOT NULL,
+  `bairro` VARCHAR(145) NOT NULL,
+  `cidade` VARCHAR(145) NOT NULL,
+  `estado` VARCHAR(45) NOT NULL,
+  `cep` VARCHAR(15) NOT NULL,
+  `telefone` VARCHAR(15) NOT NULL,
+  `celular` VARCHAR(15) NOT NULL,
+  `email` VARCHAR(145) NOT NULL,
+  `senha` VARCHAR(45) NOT NULL,
+  `status` TINYINT(1) NOT NULL DEFAULT 0,
+  `cadastrado_em` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`),
   UNIQUE KEY `CPF_Unico` (`cpf`),
   UNIQUE KEY `Email_Unico` (`email`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
---
--- Dumping data for table `clientes`
---
-
+-- Despejando dados para a tabela `clientes`
 /*!40000 ALTER TABLE `clientes` DISABLE KEYS */;
 /*!40000 ALTER TABLE `clientes` ENABLE KEYS */;
-
-
-
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
 /*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
@@ -67,4 +53,3 @@ CREATE TABLE `clientes` (
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
